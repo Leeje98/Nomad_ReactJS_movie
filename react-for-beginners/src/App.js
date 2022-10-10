d4cc8c86c27d3a73107f1a1694b8540a76f84a2f
JE <wjd7dms7@naver.com>
2022-10-10 15:40:56 +0900

    ~#6.4 ~이론

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import Button from './Button';
-import styles from './App.module.css';
+import React, { useEffect, useState } from "react";
+
+function Hello() {
+  function byFn() {
+    console.log('bye :(')
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
 
 
+  useEffect(() => {                    // 1. 화살표 함수를 이용한 방법 
+    console.log('hi :)');
+    return () => console.log('bye :(');
+  }, []);
+
+  useEffect(function(){                // 2. function을 이용한 방법
+    console.log('hi :)');
+    return function(){
+      console.log('bye :(');
+    }
+  }, [])
+  return <h1>Hello</h1>
+}
 
 function App() {
+  const [showing, setShowing] = useState(false); 
+  const onClick = () => setShowing((prev) => !prev);
   return (
-     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Continue'} />
-     </div>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+    </div>
   );
 }
+
+
 
 export default App;
